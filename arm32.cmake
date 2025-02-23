201c1ca98d92dd5343df16e87d4277e5eaa161a8
CoaguScan Project <86437593+HBL001@users.noreply.github.com>
2025-02-23 02:14:01 +0000

    Update and rename arm32.cmake to wrlinux_toolchain.cmake

--- arm32.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.10)
-
-set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_SYSTEM_PROCESSOR "aarch64")
-set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
-set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
-set(PKG_CONFIG_EXECUTABLE "aarch64-linux-gnu-pkg-config")
-set(PKGCONFIG_EXECUTABLE "aarch64-linux-gnu-pkg-config")
-set(QMAKE_EXECUTABLE "aarch64-linux-gnu-qmake")
